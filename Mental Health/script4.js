eeefb22af98303596df671ebb6943ce8f652ca9d
ShintaMeidytaPutri <shintameidytaputri@gmail.com>
2024-04-07 00:40:01 +0700

    Update2 test-page

--- Mental Health/script4.js
+++ Mental Health/script4.js
@@ -1,79 +1,46 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const questions = [
-      "Saya suka membaca buku",
-      "Saya suka menggambar",
-      "Saya suka bernyanyi",
-      "Saya suka bermain piano",
-      "Saya suka bermain bola",
-      "Saya suka bermain basket",
-      "Saya suka melukis",
-      "Saya suka bermain Video Game",
-      "Saya suka menjahit"
-  ];
+    const submitButton = document.getElementById('submit-button');
 
-  // Nilai-nilai yang terkait dengan setiap pertanyaan
-  const questionValues = [2, 3, 4, 5, 1, 2, 3, 4, 5];
+    submitButton.addEventListener('click', function() {
+        const interpretation = interpretResult();
+        renderResult(interpretation);
+    });
 
-  const questionContainer = document.getElementById('question-container');
-  const resultContainer = document.getElementById('result-container');
-  const resultDiv = document.getElementById('result');
-  const submitButton = document.getElementById('submit-button');
+    function interpretResult() {
+        const questions = document.querySelectorAll('input[type="checkbox"][name="questions"]');
+        let totalScore = 0;
 
-  //Render pertanyaan
-  questions.forEach(function(question, index) {
-      const label = document.createElement('label');
-      label.textContent = question;
-      const checkbox = document.createElement('input');
-      checkbox.setAttribute('type', 'checkbox');
-      checkbox.setAttribute('id', `question${index}`);
-      // Setiap checkbox bernilai sesuai dengan nilai yang terkait
-      checkbox.setAttribute('value', questionValues[index]);
-      const br = document.createElement('br');
-      questionContainer.appendChild(label);
-      questionContainer.appendChild(checkbox);
-      questionContainer.appendChild(br);
-  });
+        questions.forEach(function(checkbox) {
+            if (checkbox.checked) {
+                totalScore += parseInt(checkbox.value);
+            }
+        });
 
-  //Kirim jawaban
-  submitButton.addEventListener('click', function() {
-      const interpretation = interpretResult();
-      renderResult(interpretation);
-  });
+        if (totalScore >= 5 && totalScore <= 10) {
+            return { text: "Kamu gemar bermain.", imageSrc: "hasil-buku2.png" };
+        } else if (totalScore >= 11 && totalScore <= 15) {
+            return { text: "Kamu gemar bermusik.", imageSrc: "hasil-buku2.png" };
+        } else if (totalScore >= 16 && totalScore <= 20) {
+            return { text: "Kamu gemar melukis.", imageSrc: "hasil-buku2.png" };
+        } else if (totalScore >= 21 && totalScore <= 25) {
+            return { text: "Kamu gemar membaca buku.", imageSrc: "hasil-buku2.png" };
+        } else if (totalScore >= 26 && totalScore <= 30) {
+            return { text: "Kamu gemar bernyanyi.", imageSrc: "hasil-buku2.png" };
+        }
+    }
 
-  //Ketentuan hasil
-  function interpretResult() {
-      let totalScore = 0;
-      questions.forEach(function(_, index) {
-          const checkbox = document.getElementById(`question${index}`);
-          if (checkbox.checked) {
-              totalScore += parseInt(checkbox.value); // Tambahkan nilai checkbox yang dicentang
-          }
-      });
+    function renderResult(interpretation) {
+        const resultDiv = document.getElementById('result');
+        resultDiv.innerHTML = '';
+        const image = document.createElement('img');
+        image.src = interpretation.imageSrc;
+        image.alt = 'Hasil';
+        resultDiv.appendChild(image);
+        const textDiv = document.createElement('div');
+        textDiv.textContent = interpretation.text;
+        resultDiv.appendChild(textDiv);
 
-      if (totalScore >= 5 && totalScore <= 10) {
-          return { text: "Kamu gemar bermain.", imageSrc: "hasil-buku2.png" };
-      } else if (totalScore >= 11 && totalScore <= 15) {
-          return { text: "Kamu gemar mendengarkan musik.", imageSrc: "hasil-buku2.png" };
-      } else if (totalScore >= 16 && totalScore <= 20) {
-          return { text: "Kamu gemar melukis.", imageSrc: "hasil-buku2.png" };
-      } else if (totalScore >= 21 && totalScore <= 25) {
-          return { text: "Kamu memiliki minat dalam membaca buku.", imageSrc: "hasil-buku2.png" };
-      } else if (totalScore >= 26 && totalScore <= 30) {
-          return { text: "Kamu gemar menyanyi.", imageSrc: "hasil-buku2.png" };
-      }
-  }
-
-  //Render hasil
-  function renderResult(interpretation) {
-      resultDiv.innerHTML = '';
-      const image = document.createElement('img');
-      image.src = interpretation.imageSrc;
-      image.alt = 'Hasil';
-      resultDiv.appendChild(image);
-      const textDiv = document.createElement('div');
-      textDiv.textContent = interpretation.text;
-      resultDiv.appendChild(textDiv);
-
-      resultContainer.style.display = 'block';
-  }
+        const resultContainer = document.getElementById('result-container');
+        resultContainer.style.display = 'block';
+    }
 });
