464e6a8680290453cf072b077770dc3a9608cfe9
ShintaMeidytaPutri <shintameidytaputri@gmail.com>
2024-04-08 15:55:53 +0700

    test-page (update 3), home-page (footer), smooth navbar

--- Mental Health/script4.js
+++ Mental Health/script4.js
@@ -1,5 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     const submitButton = document.getElementById('submit-button');
+    const checkboxes = document.querySelectorAll('input[type="checkbox"][name="questions"]');
+    let checkedCount = 0;
+
+    checkboxes.forEach(function(checkbox) {
+        checkbox.addEventListener('change', function() {
+            if (this.checked) {
+                if (checkedCount >= 2) {
+                    this.checked = false;
+                } else {
+                    checkedCount++;
+                }
+            } else {
+                checkedCount--;
+            }
+        });
+    });
 
     submitButton.addEventListener('click', function() {
         const interpretation = interpretResult();
@@ -7,25 +23,26 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     function interpretResult() {
-        const questions = document.querySelectorAll('input[type="checkbox"][name="questions"]');
         let totalScore = 0;
 
-        questions.forEach(function(checkbox) {
+        checkboxes.forEach(function(checkbox) {
             if (checkbox.checked) {
                 totalScore += parseInt(checkbox.value);
             }
         });
 
-        if (totalScore >= 5 && totalScore <= 10) {
-            return { text: "Kamu gemar bermain.", imageSrc: "hasil-buku2.png" };
-        } else if (totalScore >= 11 && totalScore <= 15) {
-            return { text: "Kamu gemar bermusik.", imageSrc: "hasil-buku2.png" };
-        } else if (totalScore >= 16 && totalScore <= 20) {
-            return { text: "Kamu gemar melukis.", imageSrc: "hasil-buku2.png" };
-        } else if (totalScore >= 21 && totalScore <= 25) {
+        if (totalScore >= 1 && totalScore <= 2) {
             return { text: "Kamu gemar membaca buku.", imageSrc: "hasil-buku2.png" };
-        } else if (totalScore >= 26 && totalScore <= 30) {
-            return { text: "Kamu gemar bernyanyi.", imageSrc: "hasil-buku2.png" };
+        } else if (totalScore >= 3 && totalScore <= 4) {
+            return { text: "Kamu gemar seni gambar.", imageSrc: "hasil-gambar.png" };
+        } else if (totalScore >= 5 && totalScore <= 6) {
+            return { text: "Kamu gemar dalam olahraga.", imageSrc: "hasil-olahraga.png" };
+        } else if (totalScore >= 6 && totalScore <= 8) {
+            return { text: "Kamu gemar bermain Video Game.", imageSrc: "hasil-game.png" };
+        } else if (totalScore >= 9 && totalScore <= 10) {
+            return { text: "Kamu gemar merajut pakian.", imageSrc: "hasil-jahit.png" };
+        } else if (totalScore >= 11 && totalScore <= 12) {
+        return { text: "Kamu gemar dalam seni musik.", imageSrc: "hasil-musik.png" };
         }
     }
 
